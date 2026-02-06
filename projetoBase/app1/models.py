754a222694125d17a25dafa133f95b238da04d26
Marcos <msousa.nw@gmail.com>
2026-02-06 18:42:31 -0400

    criando classe model

--- projetoBase/app1/models.py
+++ projetoBase/app1/models.py
@@ -1,3 +1,100 @@
 from django.db import models
 
-# Create your models here.
+class ModelBase(models.Model):
+    id = models.BigAutoField(
+        db_column='id',
+        null=False,
+        primary_key=True
+    )
+    created_at = models.DateTimeField(
+        db_column='dt_created',
+        auto_now_add=True,
+        null=True
+    )
+    modified_at = models.DateTimeField(
+        db_column='dt_modified',
+        auto_now=True,
+        null=True
+    )
+    active = models.BooleanField(
+        db_column='cs_active',
+        null=False,
+        default=True
+    )
+
+    class Meta:
+        abstract = True
+        managed = True
+
+
+class Client(ModelBase):
+    name = models.CharField(
+        db_column='description',
+        max_length=70,
+        null=False
+    )
+    age = models.IntegerField(
+        db_column='age',
+        null=False
+    )
+    rg = models.CharField(
+        db_column='rg',
+        max_length=12,
+        null=False
+    )
+    cpf = models.CharField(
+        db_column='cpf',
+        max_length=12,
+        null=False
+    )
+
+
+class Product(ModelBase):
+    description = models.TextField(
+        db_column='tx_descricao',
+        null=False
+    )
+    quantity = models.IntegerField(
+        db_column='nb_quantidade',
+        null=False,
+        default=0
+    )
+
+
+class Employee(ModelBase):
+    name = models.CharField(
+        db_column='tx_nome',
+        max_length=70,
+        null=False
+    )
+    registration = models.CharField(
+        db_column='tx_registro',
+        max_length=15,
+        null=False
+    )
+
+
+class Sale(ModelBase):
+    product = models.ForeignKey(
+        Product,
+        db_column='id_product',
+        null=False,
+        on_delete=models.DO_NOTHING
+    )
+    client = models.ForeignKey(
+        Client,
+        db_column='id_client',
+        null=False,
+        on_delete=models.DO_NOTHING
+    )
+    employee = models.ForeignKey(
+        Employee,
+        db_column='id_employee',
+        null=False,
+        on_delete=models.DO_NOTHING
+    )
+    nrf = models.CharField(
+        db_column='tx_nrf',
+        max_length=255,
+        null=False
+    )
