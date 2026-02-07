b05fe87d6ec3ea2f11dd16a0e155d7ab3e322914
Marcos <msousa.nw@gmail.com>
2026-02-06 20:37:40 -0400

    iniciando estudos com serializers

---
+++ projetoBase/app1/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from models import Client
+
+class ClientSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=70)
+    age = serializers.IntegerField(
+        min_value=18,
+        max_value=100
+    )Import "rest_framework" 
+
+    class Meta:
+        model = Client
+        fields = ['id', 'name', 'age', 'created_at']
